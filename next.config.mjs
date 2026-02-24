0b22159128353c0f8e4f1c3a6d5fce7cd7554960
Ahiya9692 <ahiyafiverr@gmail.com>
2026-02-25 01:18:46 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'files.sitestatic.net',
-        port: '',
-        pathname: '',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
